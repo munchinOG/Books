14cb4014ce505a8f55bc71782fb52975721c7c22
Michael Etim <michaelmunchin89@gmail.com>
2020-12-01 13:21:05 +0100

    Added view component with its params

---
+++ Munchin.BookStore/Munchin.BookStore/Components/TopBooksViewComponent.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Munchin.BookStore.Repository;
+using System.Threading.Tasks;
+
+namespace Munchin.BookStore.Components
+{
+    public class TopBooksViewComponent : ViewComponent
+    {
+        private readonly BookRepositry _bookRepositry;
+
+        public TopBooksViewComponent( BookRepositry bookRepositry )
+        {
+            _bookRepositry = bookRepositry;
+        }
+        public async Task<IViewComponentResult> InvokeAsync( int count )
+        {
+            var books = await _bookRepositry.GetTopBooksAsync( count );
+            return View( books );
+        }
+    }
+}
